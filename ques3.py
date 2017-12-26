921c3b1801bbef9073e3e11ea56d9d99bca1a722
IshaShrestha <eeshashrestha18@gmail.com>
2017-12-26 11:18:58 +0545

    Characters in the sentence capitalized.

---
+++ ques3.py
@@ -0,0 +1,13 @@
+#Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+#Suppose the following input is supplied to the program:
+#Hello world
+#Then, the output should be:
+#HELLO WORLD
+
+name_str = "my name is isha shrestha."
+
+print(name_str)
+
+print("Capitalized string:")
+
+print(name_str.upper())
